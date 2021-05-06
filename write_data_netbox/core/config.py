6fcec0e3de89fd9c379d502beadb27ab5db11153
hungviet99 <44610020+hungviet99@users.noreply.github.com>
2021-05-06 22:10:53 +0700

    Tổ chức lại thư mục

--- write_data_netbox/core/config.py
+++ write_data_netbox/core/config.py
@@ -1,5 +1,5 @@
-PWD_FILE_CSV="./netbox_devices.csv"
-PWD_FILE_JSON="./file.json"
+PWD_FILE_CSV="../file_data/netbox_devices.csv"
+PWD_FILE_JSON="../file_data/file.json"
 # Đường dẫn truy cập netbox
 URL_NB ="http://10.10.35.191:8000"
 # Token của tài khoản admin netbox
