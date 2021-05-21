f929bcb23ba99eef44c6fe1a6c754640b1ecdce7
hungviet99 <44610020+hungviet99@users.noreply.github.com>
2021-05-21 16:19:18 +0700

    cập nhật động cho tên các file sheet

--- netbox_create_data/core/config.py
+++ netbox_create_data/core/config.py
@@ -1,3 +1,6 @@
+"""
+Cấu hình đường dẫn excel và các file json
+"""
 # Đường dẫn file excel
 NETBOX_INFO_EXCEL = '../file_data/netbox_info.xlsx'
 # Đường dẫn file json chứa thông tin region và site
@@ -18,6 +21,32 @@ AGGREGATES_JSON = '../file_data/aggregates.json'
 INTERFACE_TPL = '../file_data/interface_templates.json'
 # Đường dẫn file json chứa thông tin về cable
 CABLE_CONNECT = '../file_data/cable_connections.json'
+
+"""
+Cấu hình tên sheet trong excel
+"""
+# Sheet chứa thông tin region và site
+regions_sites = "Region va Site"
+# Sheet chứa thông tin racks
+racks = "racks"
+# Sheet chứa thông tin device type
+device_types = "Kieu thiet bi"
+# Sheet chứa thông tin các template của interface
+inf_template = "interface_templates"
+# Sheet chứa thông tin deivce role
+device_roles = "Vai tro thiet bi"
+# Sheet chứa thông tin device
+devices = "Thiet bi"
+# Sheet chứa thông tin aggregates
+aggregates= "Aggregates"
+# Sheet chứa thông tin vlan 
+vlans = "vlans"
+# Sheet chứa thông tin các kết nối, địa chỉ ip
+cable_ip = "Thong tin IP"
+
+"""
+Cấu hình truy cập netbox
+"""
 # Đường dẫn truy cập netbox
 URL_NB ="http://10.10.35.191:8000"
 # Token của tài khoản admin netbox
--- netbox_create_data/core/convert_csv_to_json.py
+++ netbox_create_data/core/convert_csv_to_json.py
@@ -1,6 +1,11 @@
+'''
+Module này chứa các funtion để convert thông tin các sheet trong file excel thành json
+    sau đó lưu vào các file json đã tạo trước đó.
+'''
 import pandas as ps
 import config
 
+# Lấy file excel và các file json từ config
 netbox_excel_data = config.NETBOX_INFO_EXCEL
 regions_sites_json=config.REGION_SITE_JSON
 racks_json=config.RACK_JSON
@@ -12,15 +17,23 @@ interface_tpl_json=config.INTERFACE_TPL
 cable_connections_json=config.CABLE_CONNECT
 vlans_json=config.VLANS_JSON
 
-'''
-Module này chứa các funtion để convert thông tin các sheet trong file excel thành json
-    sau đó lưu vào các file json đã tạo trước đó.
-'''
+# Lấy thông tin về tên sheet
+regions_sites = config.regions_sites
+racks = config.racks
+device_types = config.device_types
+inf_template = config.inf_template
+device_roles = config.device_roles
+devices = config.devices
+aggregates= config.aggregates
+vlans = config.vlans
+cable_ip = config.cable_ip
+
+# Các hàm chuyển excel sang json 
 
 def convert_region_site():
     try:
         excel_data_df = ps.read_excel('{}' .format(netbox_excel_data),
-                                      sheet_name='Region va Site',
+                                      sheet_name='{}' .format(regions_sites),
                                       engine='openpyxl')
         excel_data_df.to_json('{}' .format(regions_sites_json))
     except Exception as ex:
@@ -30,7 +43,7 @@ def convert_region_site():
 def convert_rack():
     try:
         excel_data_df = ps.read_excel('{}' .format(netbox_excel_data),
-                                      sheet_name='racks',
+                                      sheet_name='{}' .format(racks),
                                       engine='openpyxl')
         excel_data_df.to_json('{}' .format(racks_json))
     except Exception as ex:
@@ -40,7 +53,7 @@ def convert_rack():
 def convert_device_type():
     try:
         excel_data_df = ps.read_excel('{}' .format(netbox_excel_data),
-                                      sheet_name='Kieu thiet bi',
+                                      sheet_name='{}' .format(device_types),
                                       engine='openpyxl')
         excel_data_df.to_json('{}' .format(device_types_json))
     except Exception as ex:
@@ -50,7 +63,7 @@ def convert_device_type():
 def convert_device_role():
     try:
         excel_data_df = ps.read_excel('{}' .format(netbox_excel_data),
-                                      sheet_name='Vai tro thiet bi',
+                                      sheet_name='{}' .format(device_roles),
                                       engine='openpyxl')
         excel_data_df.to_json('{}' .format(device_roles_json))
     except Exception as ex:
@@ -60,7 +73,7 @@ def convert_device_role():
 def convert_device():
     try:
         excel_data_df = ps.read_excel('{}' .format(netbox_excel_data),
-                                      sheet_name='Thiet bi',
+                                      sheet_name='{}' .format(devices),
                                       engine='openpyxl')
         excel_data_df.to_json('{}' .format(devices_json))
     except Exception as ex:
@@ -70,7 +83,7 @@ def convert_device():
 def convert_vlans():
     try:
         excel_data_df = ps.read_excel('{}' .format(netbox_excel_data),
-                                      sheet_name='vlans',
+                                      sheet_name='{}' .format(vlans),
                                       engine='openpyxl')
         excel_data_df.to_json('{}' .format(vlans_json))
     except Exception as ex:
@@ -80,7 +93,7 @@ def convert_vlans():
 def convert_aggregates():
     try:
         excel_data_df = ps.read_excel('{}' .format(netbox_excel_data),
-                                      sheet_name='Aggregates',
+                                      sheet_name='{}' .format(aggregates),
                                       engine='openpyxl')
         excel_data_df.to_json('{}' .format(aggregates_json))
     except Exception as ex:
@@ -100,7 +113,7 @@ def convert_aggregates():
 def convert_inf_tpl():
     try:
         excel_data_df = ps.read_excel('{}' .format(netbox_excel_data),
-                                      sheet_name='interface_templates',
+                                      sheet_name='{}' .format(inf_template),
                                       engine='openpyxl')
         excel_data_df.to_json('{}' .format(interface_tpl_json))
     except Exception as ex:
@@ -110,7 +123,7 @@ def convert_inf_tpl():
 def convert_cable_connect():
     try:
         excel_data_df = ps.read_excel('{}' .format(netbox_excel_data),
-                                      sheet_name='Thong tin IP',
+                                      sheet_name='{}' .format(cable_ip),
                                       engine='openpyxl')
         excel_data_df = excel_data_df.fillna(method='ffill', axis=0)
         excel_data_df.to_json('{}' .format(cable_connections_json))
@@ -127,4 +140,3 @@ def convert_cable_connect():
 #     except Exception as ex:
 #         print(ex)
 #     return
-
